4495d0d07ad7e33ae2fb75594fe9e8094654c33c
sebastian-napora <86401150+sebastian-napora@users.noreply.github.com>
2022-11-29 09:34:47 +0100

    feat: ATOMIC-15-set-encoded-cookie-token, added encoded token, created new docs folder (#18)
    Co-authored-by: Sebastian Napora <sna@spyro-soft.com>

---
+++ src/api/utils.ts
@@ -0,0 +1,5 @@
+import jwt_decode from 'jwt-decode';
+
+export const TOKEN = jwt_decode(
+  'eyJhbGciOiJIUzI1NiJ9.ImdocF9VbGtxbzdNTUlUaWtlanV1TEZxU050N3FDd2dUbW4xaDZLUkMi.q6at8Ayn4AQuQGFmys-7DyTGG4_MfoXDhq5ukFlHlEM'
+);
