76b17403f6490b1e9953b2ba8fc7d3351c6f360c
sebastian-napora <86401150+sebastian-napora@users.noreply.github.com>
2022-12-01 07:12:16 +0100

    refactor: ATOMIC-12-theme-styled-components-folder, updated types for useTheme custom hook, and clean GlobalStyles from not necessary styles (#15)
    Co-authored-by: Sebastian Napora <sna@spyro-soft.com>

--- src/theme/useTheme.ts
+++ src/theme/useTheme.ts
@@ -2,16 +2,27 @@ import { useEffect, useState } from 'react';
 
 import { THEMES } from './schema';
 
-import { TSingleTheme, TThemes, TUseTheme } from './types';
+import { TSingleTheme, TUseTheme } from './types';
 
-export const useTheme = (): TUseTheme => {
+export const useTheme: TUseTheme = () => {
   const allThemes = THEMES;
+  //useStates
   const [theme, setTheme] = useState<TSingleTheme>(allThemes.data.main);
-  const [themeLoaded, setThemeLoaded] = useState<boolean>(false);
+  const [themeLoaded, setThemeLoaded] = useState(false);
 
-  const setMode = (mode: TSingleTheme): void => setTheme(mode);
+  /**
+   * @description This function manage a theme which we want to display. After change theme styles should be changed.
+   * @function setMode
+   * @category Theme
+   */
+  const setMode: ReturnType<TUseTheme>['setMode'] = (mode) => setTheme(mode);
 
-  const handleSetTheme = (localThemes: TThemes): void => {
+  /**
+   * @description This function sets a theme which we want to display. After change theme styles should be changed.
+   * @function handleSetTheme
+   * @category Theme
+   */
+  const handleSetTheme: ReturnType<TUseTheme>['handleSetTheme'] = (localThemes) => {
     localThemes && setTheme(localThemes.data.main);
     setThemeLoaded(true);
   };
